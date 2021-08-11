58b1eed3c6183a2be23fe834d90766099f451d58
maingo <ngothingocmaiclht@gmail.com>
2021-08-12 01:37:58 +0700

    Create PlaceDetails Card

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,7 +1,112 @@
 import React from "react";
 
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Chip,
+} from "@material-ui/core";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PhoneIcon from "@material-ui/icons/Phone";
+import Rating from "@material-ui/lab/Rating";
+
+import useStyles from "./styles";
+
 const PlaceDetails = ({ place }) => {
-  return <h1>{place.name}</h1>;
+  const classes = useStyles();
+
+  return (
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 400 }}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : "https://media-cdn.tripadvisor.com/media/photo-s/1b/67/cc/f8/chestnut-restaurant.jpg"
+        }
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          {place.name}
+        </Typography>
+
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Price</Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            {place.price_level}
+          </Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Ranking</Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            {place.ranking}
+          </Typography>
+        </Box>
+
+        {place?.awards?.map((award) => (
+          <Box
+            my={1}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <img src={award.images.small} alt={award.display_name} />
+
+            <Typography variant="subtitle2" color="textSecondary">
+              {award.display_name}
+            </Typography>
+          </Box>
+        ))}
+
+        {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size="small" label={name} className={classes.chip} />
+        ))}
+
+        {place?.address && (
+          <Typography
+            gutterBottom
+            variant="body2"
+            color="textSecondary"
+            className={classes.subtitle}
+          >
+            <LocationOnIcon /> {place.address}
+          </Typography>
+        )}
+        {place?.address && (
+          <Typography
+            gutterBottom
+            variant="subtitle"
+            color="textSecondary"
+            className={classes.spacing}
+          >
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
+      </CardContent>
+
+      <CardActions>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => window.open(place.web_url, "_blank")}
+        >
+          Trip Advisor
+        </Button>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => window.open(place.website, "_blank")}
+        >
+          Website
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default PlaceDetails;
